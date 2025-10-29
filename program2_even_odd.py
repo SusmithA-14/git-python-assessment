38fad1fd5e85da7ffdc866c305e4e7780ecbbacd
SusmithA-14 <susmithadannana22002@gmail.com>
2025-10-29 11:05:19 +0530

    check even or odd

---
+++ program2_even_odd.py
@@ -0,0 +1,10 @@
+# Program to check if a number is odd or even
+
+# Get input from user
+num = int(input("Enter a number: "))
+
+# Check if number is even or odd
+if num % 2 == 0:
+    print(num, "is even")
+else:
+    print(num, "is odd")
